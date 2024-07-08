ddd929d860fa028146cabbbeadb3acaf89840e8f
rghvsh <116428320+rghvsh@users.noreply.github.com>
2024-07-08 13:21:12 +0530

    Update pytfile.py

--- pytfile.py
+++ pytfile.py
@@ -3,8 +3,51 @@ from pinecone import ServerlessSpec
 
 pc = Pinecone(api_key='')
 
+import torch
+import torch.nn.functional as F
+from transformers import AutoTokenizer, AutoModel
+import tensorflow 
+import pandas as pd
 
-index_name = "docs-quickstart-index"
+# Each query needs to be accompanied by an corresponding instruction describing the task.
+task_name_to_instruct = {"example": "Given a question, retrieve passages that answer the question",}
+
+query_prefix = "Instruct: "+task_name_to_instruct["example"]+"\nQuery: "
+queries = [
+    'who messaged me today'
+    'what are my meetings today', 
+    ]
+
+# No instruction needed for retrieval passages
+passage_prefix = ""
+passages = [
+    "messages from a Intel",
+    "meeting at noon tommorow"
+]
+
+# load model with tokenizer
+model = AutoModel.from_pretrained('nvidia/NV-Embed-v1', trust_remote_code=True)
+
+# get the embeddings
+max_length = 4096
+query_embeddings = model.encode(queries, instruction=query_prefix, max_length=max_length)
+
+passage_embeddings = model.encode(passages, instruction=passage_prefix, max_length=max_length)
+
+# normalize embeddings
+query_embeddings = F.normalize(query_embeddings, p=2, dim=1)
+passage_embeddings = F.normalize(passage_embeddings, p=2, dim=1)
+
+# get the embeddings with DataLoader (spliting the datasets into multiple mini-batches)
+# batch_size=2
+# query_embeddings = model._do_encode(queries, batch_size=batch_size, instruction=query_prefix, max_length=max_length, num_workers=32)
+# passage_embeddings = model._do_encode(passages, batch_size=batch_size, instruction=passage_prefix, max_length=max_length, num_workers=32)
+
+scores = (query_embeddings @ passage_embeddings.T) * 100
+print(scores.tolist())
+#[[77.9402084350586, 0.4248958230018616], [3.757718086242676, 79.60113525390625]]
+
+index_name = "retrieval"
 
 if index_name not in pc.list_indexes().names():
     pc.create_index(
