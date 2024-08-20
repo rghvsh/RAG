44ed71fd51b9454098d8da7e14ffaa3816b3ea6a
rghvsh <116428320+rghvsh@users.noreply.github.com>
2024-08-20 17:49:30 +0530

    Create upsert.py

---
+++ upsert.py
@@ -0,0 +1,49 @@
+import telethon
+from telethon.sync import TelegramClient, events 
+from telethon.sync import TelegramClient
+from sentence_transformers import SentenceTransformer
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+
+api_id = int(input("Enter telegram api_id"))
+api_hash = input("Enter api_hash")
+index_name = input("Enter index name")
+chat = input("Enter chat for which you want to retrieve messages")
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+api_key = input("Enter the api_key for pinecone")
+
+pc = Pinecone(api_key= api_key)
+
+index_name = input("Enter index name for pinecone which you want to set")
+
+if index_name not in pc.list_indexes().names():
+    pc.create_index(
+        name=index_name,
+        dimension= 384,
+        metric="cosine",
+        spec=ServerlessSpec(
+            cloud='aws', 
+            region='us-east-1'
+        ) 
+    ) 
+ 
+vectors1 = []
+metadata= []
+with TelegramClient("fetch1", api_id, api_hash) as client:
+    for message in client.iter_messages(chat):
+        a = message.text + " "+ str(message.date) + ' ' + str(message.media_unread)
+        metadata.append(a)
+        xq = model.encode(a).tolist()
+        vectors1.append(xq)
+
+index = pc.Index(index_name)
+
+for vectorx in len(vectors1):
+    index.upsert(
+        vectors=[
+            {"id": str(vectorx),
+              "values": vectors1[vectorx],
+              "metadata": {"text": metadata[vectorx] }
+            },
+    ],
+   )
